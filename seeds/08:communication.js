11724d37a2ec61bdc7685f8ad7b186e83e9ff0b3
Caroline O'Donnell <caroline.odonnell26@gmail.com>
2017-08-25 16:19:25 -0600

    debugging id seq

--- seeds/08:communication.js
+++ seeds/08:communication.js
@@ -1,11 +1,12 @@
 
 
 exports.seed = function(knex, Promise) {
-  return knex.raw('TRUNCATE communication CASCADE; ALTER SEQUENCE communication_id_seq restart with 6')
+  return knex.raw('ALTER SEQUENCE communication_id_seq restart;')
     .then(function () {
-      return knex('communication').insert([
+      return knex('communication').del().then(function() {
+        return Promise.all([
+          knex('communication').insert([
         {
-          id: 1,
           communication_type_id:4,
           raw_body:`Hey it's Little Bird from Hogwarts Elementary! Your child's IEP review is coming up. Whay days during the week of September 4th would you be available to meet for an hour? For example, type 'Monday, Tuesday and Wednesday'`,
           timestamp: '1502069312345',
@@ -13,7 +14,6 @@ exports.seed = function(knex, Promise) {
           stakeholder_id: 7
         },
         {
-          id: 2,
           communication_type_id:1,
           raw_body:'Monday or Wednesday',
           timestamp: '1502069313345',
@@ -21,7 +21,6 @@ exports.seed = function(knex, Promise) {
           stakeholder_id: 2
         },
         {
-          id: 3,
           communication_type_id:4,
           raw_body:'Great! Do mornings or afternoons tend to work better for you?',
           timestamp: '1502069314345',
@@ -29,7 +28,6 @@ exports.seed = function(knex, Promise) {
           stakeholder_id: 7
         },
         {
-          id: 4,
           communication_type_id:1,
           raw_body:'Mornings',
           timestamp: '1502069315345',
@@ -37,13 +35,14 @@ exports.seed = function(knex, Promise) {
           stakeholder_id: 2
         },
         {
-          id: 5,
           communication_type_id:4,
           raw_body:'Awesome! We will be in touch soon with potential times.',
           timestamp: '1502069316345',
           student_id: 1,
           stakeholder_id: 7
         }
-      ]);
-    });
+      ])
+    ])
+      })
+    })
 };
