5c6572f03bd94ed3431fd590a57e4babee264a6b
Caroline O'Donnell <caroline.odonnell26@gmail.com>
2017-08-31 11:06:17 -0600

    Adding ED

--- seeds/09:guardian_availability.js
+++ seeds/09:guardian_availability.js
@@ -4,7 +4,7 @@ exports.seed = function(knex, Promise) {
       return knex('guardian_availability').insert([
         {stakeholder_id: 2, date: 'Monday', timeframe: 'morning'},
         {stakeholder_id: 2, date: 'Thursday', timeframe: 'morning'},
-        {stakeholder_id: 2, date: 'Friday', timeframe: 'morning'}
+        //{stakeholder_id: 2, date: 'Friday', timeframe: 'morning'}
       ]);
     });
 };
